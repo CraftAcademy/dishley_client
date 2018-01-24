7d6be10e647891425fab92c28b96d3f149423a99
Francesco <francesco.falchi@gmail.com>
2018-01-24 22:37:46 +0100

    Set up app component and restaurants provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,8 +16,16 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+              public statusBar: StatusBar,
+              public splashScreen: SplashScreen,
+              public _tokenService: Angular2TokenService
+              ) {
     this.initializeApp();
+
+    this._tokenService.init({
+      apiBase: 'https://jak-sie-masz-slowfood.herokuapp.com/api'
+    })
 
     this.pages = [
       { title: 'Home', component: HomePage },
--- src/providers/restaurants/restaurants.ts
+++ src/providers/restaurants/restaurants.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Angular2TokenService } from 'angular2-token';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class PerfomanceDataProvider {
-  constructor(private _tokenService: Angular2TokenService) {}
+export class RestaurantsProvider {
+  private apiUrl: string;
 
-  saveData(data) {
-    return this._tokenService.post('performance_data', data).map(data => data);
+  constructor(public http: HttpClient) {
+    console.log('Hello Provider, here are some restaurants');
+    this.apiUrl = 'https://jak-sie-masz-slowfood.herokuapp.com/api'
   }
+
+  all() : Observable<any> {
+    return this.http.get(this.apiUrl + '/restaurants')
+  }
+
 }
