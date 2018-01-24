8ce3739887a05ec949635166ac413e2c52b597f6
Francesco <francesco.falchi@gmail.com>
2018-01-24 22:56:04 +0100

    Set up home ts to show restaurants, need cors gem in back-end

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { RestaurantsProvider } from './../../providers/restaurants/restaurants';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +7,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public restaurants :any
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    public restaurantsProvider: RestaurantsProvider,
+  ) {
+    this.restaurantsProvider.all().subscribe(({ data }) => {
+      this.restaurants = data;
+      console.log(this.restaurants);
+    })
   }
 
 }
