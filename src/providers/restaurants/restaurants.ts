8b683ecbaf91ec8ccaab8381cef0384cc1495007
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-01-25 17:56:19 +0100

    Binds restaurants to the view

--- src/providers/restaurants/restaurants.ts
+++ src/providers/restaurants/restaurants.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+
 import { Angular2TokenService } from 'angular2-token';
 
 @Injectable()
