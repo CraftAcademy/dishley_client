3460ac1bd3a448877219023f657b9e1aa090f656
Francesco <francesco.falchi@gmail.com>
2018-01-25 11:23:49 +0100

    Add map viewchild and google var to home ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+@ViewChild('map') mapRef: ElementRef;
+map: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
-
+ionViewDidLoad() {console.log(this.mapRef);}
 }
